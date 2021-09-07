c8b49adced2665a741f8f0a030c6811ced33f7da
JIshanRandika <jishanrandika@gmail.com>
2021-09-07 12:54:10 +0530

    ingredient model

---
+++ back_end/app/models/ingredient.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const IngredientSchema = mongoose.Schema({
+    ingredientName: String,
+    status: String,
+    description: String,
+    alternate:String,
+    image:String
+
+});
+
+module.exports = mongoose.model('Ingredient', IngredientSchema);
